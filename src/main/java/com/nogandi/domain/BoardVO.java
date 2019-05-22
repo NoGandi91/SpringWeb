612d9a999030479e713f1990c38798f00818a582
NoHong <shghdgusa@naver.com>
2019-05-22 11:07:22 +0900

    0522

---
+++ src/main/java/com/nogandi/domain/BoardVO.java
@@ -0,0 +1,56 @@
+package com.nogandi.domain;
+
+import java.util.Date;
+
+public class BoardVO {
+	
+	private Integer bno;	//고유번호
+	private String title;	//제목
+	private String content;	//내용
+	private String writer;	//작성자
+	private Date regdate;	//작성일자
+	private int viewCnt;	//조회수
+	public Integer getBno() {
+		return bno;
+	}
+	public void setBno(Integer bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getViewCnt() {
+		return viewCnt;
+	}
+	public void setViewCnt(int viewCnt) {
+		this.viewCnt = viewCnt;
+	}
+	@Override
+	public String toString() {
+		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
+				+ regdate + ", viewCnt=" + viewCnt + "]";
+	}
+	
+	
+}
